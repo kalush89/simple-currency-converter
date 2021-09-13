f15b76662c95a8aa05ed96821394fc6d3d7354b5
rkterungwa16 <kombolalbert@gmail.com>
2021-09-13 22:14:27 +0100

    Fix currency convertion issue

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 class Converter {
     constructor(amount, from, to) {
-        this.amont = amount;
+        this.amount = amount;
         this.from = from;
         this.to = to;
     }
@@ -8,16 +8,27 @@ class Converter {
     //Validate
     validate() {
         let msg = 'This field is required!';
-        if (!this.amount) {
-            this.outputErr(msg, 'amount');
-        } else if (!this.from) {
-            this.outputErr(msg, 'from');
-        } else if (!this.to) {
-            this.outputErr(msg, 'to');
+        console.log(this.amount);
+        if (!this.amount || !this.from || !this.to) {
+            this.output(msg, 'err');
+            return;
         } else {
+            console.log('good', this.amount);
             this.convert();
+            this.output(' ', 'err');
         }
 
+    }
+
+     //get element
+     getElement(id) {
+        return document.getElementById(id);
+    }
+
+    //Output
+    output(msg, id) {
+        let display = this.getElement(id);
+        display.innerText = msg;
     }
 
     //encode
@@ -34,34 +45,45 @@ class Converter {
         let currStream = await fetch(`https://free.currconv.com/api/v7/currencies?apiKey=16ef1ff68cbc6fe7bdae`);
         let currencies = await currStream.json();
         for (const currency of Object.values(currencies.results)) {
-
+            
             this.appendOptns(parentFrom, parentTo, currency);
 
         }
     }
 
-    //get element
-    getElement(id) {
-        return document.getElementById(id);
-    }
+    //Shorten Select options
+    truncateOptns(txt) {
+        if (txt.length > 25) {
+            return txt.slice(0, 25) + '...';
+        } else {
+            return txt;
+        }
+
+    }   
 
     //Append currency options
     appendOptns(parentFrom, parentTo, content) {
         const childNodeFrom = document.createElement('option');
         const childNodeTo = document.createElement('option');
-        childNodeFrom.innerHTML = content.currencyName;
-        childNodeTo.innerHTML = content.currencyName;
+
+        childNodeFrom.innerHTML = this.truncateOptns(content.currencyName);
+        childNodeTo.innerHTML = this.truncateOptns(content.currencyName);
+
         parentFrom.append(childNodeFrom);
         parentTo.append(childNodeTo);
+
+        childNodeFrom.setAttribute('value', content.id);
+        childNodeTo.setAttribute('value', content.id);
     }
 
     //api request for rate
-    async getRate() {
+    getRate = async () => {
         try{
         let currStream = await fetch(`https://free.currconv.com/api/v7/convert?q=${this.encodeUri()}&compact=ultra&apiKey=16ef1ff68cbc6fe7bdae`);
         let rate = await currStream.json();
-console.log('running smooth')
-        return rate.this.encodeUri();
+//console.log(rate);
+
+        return rate;
         }catch(e){
             console.log(e);
         }
@@ -69,20 +91,18 @@ console.log('running smooth')
 
     //convert
     convert() {
-        try{
-            console.log('running smooth too')
-        let display = this.getElement('display');
-        let result = this.getRate * this.amount;
-        display.innerText = `Exchanges to ${result}`;
-        }catch(e){
-            console.log(e);
+        this.getRate().then(res => {
+            console.log('res',Object.values(res)[0]);
+            let rate = Object.values(res)[0];
+            let display = this.getElement('display');
+            let result = rate * this.amount;
+            display.innerText = `Exchanges to ${result}`;
+            
+        }).catch(err => console.log(err));
+    
         }
-        //console.log(result);
-    }
+       
+    
 
-    //Output
-    outputErr(msg, id) {
-        let displayErr = this.getElement(id);
-        displayErr.innerHTML = msg;
-    }
+    
 }
